2ab4882d3dcb50285a43d1683a92dbad9a5d0a3e
Aswin VB <aswinvb.aswin6@gmail.com>
2024-05-06 12:49:43 +0530

    fix: title size

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
             <div className="justify-between flex items-center space-x-4">
                 <div className="flex items-center space-x-3">
                     <Image alt="subscription-tracker" src="/budget.png" height="100" width="100" className="w-8 h-8" />
-                    <h1 className="text-2xl font-bold">Subscription Tracker</h1>
+                    <h1 className="text-xl font-bold">Subscription Tracker</h1>
                 </div>
                 <Auth />
             </div>
